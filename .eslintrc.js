fd1f8be4d2b4a9617a356e3eb48164146d98213c
小冬菇 <linchuanbin36@163.com>
2021-06-13 02:28:36 +0800

    project initialized

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,18 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "indent": [2, 2, {'SwitchCase': 1}],
+    'space-before-function-paren': 0,
+    "no-undef": 0,
+    "vue/no-unused-vars": 0,
+    "no-unused-vars": [0, {
+      // 允许声明未使用变量
+      "vars": "local",
+      // 参数不检查
+      "args": "none"
+    }],
+    // 关闭语句强制分号结尾
+    "quote-props": [0, "always"]
   }
 }
