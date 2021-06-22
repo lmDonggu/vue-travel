ed8060a3ba50ffa64edab17052c41f6f22501afb
小冬菇 <linchuanbin36@163.com>
2021-06-22 20:38:44 +0800

    ajax city 页面数据动态渲染完成

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -13,26 +13,24 @@
 
         <!-- 热门城市 -->
         <div class="area">
-            <div class="title border-topbottom">热门城市城市</div>
+            <div class="title border-topbottom">热门城市</div>
             <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
+                <div class="button-wrapper" v-for="item of hot" :key="item.id">
+                    <div class="button">{{item.name}}</div>
                 </div>
             </div>
         </div>
 
         <!-- 字母表 -->
-        <div class="area">
-            <div class="title border-topbottom">A</div>
+        <div class="area" v-for="(item, key) of cities" :key="key">
+            <div class="title border-topbottom">{{key}}</div>
             <div class="item-list">
-                <div class="item border-bottom">aerla</div>
-                <div class="item border-bottom">aerla</div>
-                <div class="item border-bottom">aerla</div>
-                <div class="item border-bottom">aerla</div>
-                <div class="item border-bottom">aerla</div>
-                <div class="item border-bottom">aerla</div>
-                <div class="item border-bottom">aerla</div>
-                <div class="item border-bottom">aerla</div>
+                <div
+                class="item border-bottom"
+                v-for="innerItem of item"
+                :key="innerItem.id">
+                  {{innerItem.name}}
+                </div>
             </div>
         </div>
     </div>
@@ -41,8 +39,13 @@
 
 <script>
 import Bscroll from 'better-scroll'
+
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
