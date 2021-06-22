47ea4e289015591d31b6c20fb31deb356174fa90
小冬菇 <linchuanbin36@163.com>
2021-06-22 16:50:05 +0800

    citylist 列表页面布局完成

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 
 <script>
+// 组件引入
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+
 export default {
   name: 'City',
+  // 挂载引入的组件
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   }
 }
 </script>
