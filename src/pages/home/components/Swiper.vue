936bd633ec34621814d4a83cd84541cefebc2fc3
小冬菇 <linchuanbin36@163.com>
2021-06-25 17:01:54 +0800

    code to simplify

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -3,7 +3,7 @@
     <swiper :options="swiperOption" v-if="showSwiper">
       <!-- slides -->
       <swiper-slide v-for ='item of list' :key="item.id">
-        <img class="swiper-img" :src="item.imgUrl" alt="">
+        <img class="swiper-img" :src="item.imgUrl" />
       </swiper-slide>
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
